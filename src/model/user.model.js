8a9845d275122b794c5c9dcc06dae76ca90593e5
Uzmaa7 <uk9971khan@gmail.com>
2026-04-04 20:11:57 +0530

    Feat: implement secure auth methods in User model
    - Add pre-save hook for bcrypt password hashing
    - Add isPasswordCorrect method for verification
    - Add generateAccessToken and generateRefreshToken methods

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import { AvailableUserRoles, AvailableUserStatus, UserRoles, UserStatus } from "../utils/constants";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 
 const userSchema = new mongoose.Schema({
 
@@ -71,6 +74,42 @@ const userSchema = new mongoose.Schema({
     }
 
 },{timestamps:true})
+
+// middleware(pre hook) to hash the password 
+userSchema.pre("save", async function(){
+    if(!this.isModified("password")) return
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function (){
+    return jwt.sign(
+        {
+            _id : this._id,
+            role : this.role
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function (){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+            
+}
 
 const User = mongoose.model("User", userSchema);
 
